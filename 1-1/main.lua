6eb7688eb38ce8c2570a5816295ffe2a2df131e8
Amy B <amy.jennifer.brown@gmail.com>
2020-12-06 13:50:20 -0500

    1-1 attempt

--- 1-1/main.lua
+++ 1-1/main.lua
@@ -5,4 +5,22 @@
 
 local file = assert( io.open('input.txt', 'r')) -- open and assert file exists
 
-local text_numbers = file.read("*all")
+local numbers = {}
+
+-- read each lines number and store it
+for i, number in ipairs(file:lines("n")) do
+    numbers[i] = number
+end
+
+for n in numbers do
+    for m in numbers do
+        if n + m == 2020 then
+            print(n*m)
+            return
+        end
+    end
+end
+
+-- if it hasn't been found, print out error and quit
+print("Couldn't find any numbers that fit!")
+return
